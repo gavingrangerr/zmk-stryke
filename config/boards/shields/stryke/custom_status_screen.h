e5b68bee0c174f21091e8826c406607c64c9a779
Gavin Granger <gavin.grangerr@gmail.com>
2026-01-23 14:19:03 -0500

    v2.0.3

--- config/boards/shields/stryke/custom_status_screen.h
+++ config/boards/shields/stryke/custom_status_screen.h
@@ -1,10 +1,15 @@
+#include <zephyr/kernel.h>
 #include <zmk/display/status_screen.h>
-#include "widgets/stryke_display.h"
+#include "stryke_display.h"
 
 static struct stryke_display_widget stryke_widget;
 
-lv_obj_t *zmk_display_status_screen() {
-    lv_obj_t *screen = lv_obj_create(NULL);
-    stryke_display_init(&stryke_widget, screen);
-    return screen;
+static int status_screen_init(lv_obj_t *screen) {
+    return stryke_display_init(&stryke_widget, screen);
 }
+
+static void status_screen_update() {
+    // Updates every second if needed
+}
+
+ZMK_DISPLAY_STATUS_SCREEN_CUSTOM(status_screen, NULL, status_screen_init, status_screen_update);
--- config/boards/shields/stryke/widgets/stryke_display.h
+++ config/boards/shields/stryke/widgets/stryke_display.h
@@ -3,21 +3,9 @@
 
 struct stryke_display_widget {
     lv_obj_t *obj;
-    
-    // Main key display area
-    lv_obj_t *key_display;
     lv_obj_t *key_label;
-    
-    // Layer indicators
-    lv_obj_t *layer_indicators[5];
     lv_obj_t *layer_labels[5];
 };
 
-// Reference key dimensions
-#define CARD_HEIGHT 36
-#define MAX_TEXT_HEIGHT 16
-
 int stryke_display_init(struct stryke_display_widget *widget, lv_obj_t *parent);
 lv_obj_t *stryke_display_obj(struct stryke_display_widget *widget);
-void stryke_display_update_layer(uint8_t layer);
-void stryke_display_update_key(uint16_t keycode, bool pressed, const char *key_name);
