336a8860f0a5dc1607ce33a655a0d2a48e6eaca3
gavingrangerr <gavin.grangerr@gmail.com>
2026-01-26 15:44:43 -0500

    v3.1.0

--- config/boards/shields/stryke/widgets/custom_status.c
+++ config/boards/shields/stryke/widgets/custom_status.c
@@ -4,34 +4,42 @@
 #include <zmk/display/widgets/battery_status.h>
 #include <lvgl.h>
 
+static lv_obj_t *screen;
+
 static struct zmk_widget_layer_status layer_status_widget;
 static struct zmk_widget_battery_status battery_status_widget;
 
-lv_obj_t *zmk_display_status_screen() {
-    lv_obj_t *screen = lv_obj_create(NULL);
+lv_obj_t *zmk_display_status_screen(void) {
+    if (screen != NULL) {
+        return screen;
+    }
 
-    // 1. Force Black Background to wipe the 'snow'
+    screen = lv_obj_create(NULL);
+
+    // Black background (good, keep this)
     lv_obj_set_style_bg_color(screen, lv_color_black(), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
 
-    // 2. Battery Widget (Top Left)
+    // Battery widget
     zmk_widget_battery_status_init(&battery_status_widget, screen);
-    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_LEFT, 4, 4);
+    lv_obj_align(
+        zmk_widget_battery_status_obj(&battery_status_widget),
+        LV_ALIGN_TOP_LEFT, 4, 4
+    );
 
-    // 3. Layer Widget (Top Right)
+    // Layer widget
     zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_RIGHT, -4, 4);
+    lv_obj_align(
+        zmk_widget_layer_status_obj(&layer_status_widget),
+        LV_ALIGN_TOP_RIGHT, -4, 4
+    );
 
-    // 4. Main Label (Center)
+    // Center title
     lv_obj_t *title_label = lv_label_create(screen);
     lv_label_set_text(title_label, "STRYKE");
-    
-    // If you add CONFIG_LV_FONT_MONTSERRAT_16=y to stryke.conf, use this:
-    // lv_obj_set_style_text_font(title_label, &lv_font_montserrat_16, LV_PART_MAIN);
-    
     lv_obj_align(title_label, LV_ALIGN_CENTER, 0, 0);
 
-    // 5. System Status (Bottom)
+    // Bottom status
     lv_obj_t *status_label = lv_label_create(screen);
     lv_label_set_text(status_label, "NEXUS PRO");
     lv_obj_align(status_label, LV_ALIGN_BOTTOM_MID, 0, -5);
