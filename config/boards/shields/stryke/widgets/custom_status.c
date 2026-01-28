fd64fffb75f8a028fda8933472b9b396207ebcd5
gavingrangerr <gavin.grangerr@gmail.com>
2026-01-27 21:25:59 -0500

    v3.7.6

--- config/boards/shields/stryke/widgets/custom_status.c
+++ config/boards/shields/stryke/widgets/custom_status.c
@@ -28,9 +28,9 @@ static int64_t last_key_time = 0;
 
 static const char* key_names[MAX_LAYERS][MAX_POSITIONS] = {
     {
-        "⌘C",           "⌘V",           "⌘X",           "⌘Z",
-        "⌘A",           "⌘S",           "⌘F",           "⌘⇧Z",
-        "⌘⇧3",          "⌘⇧4",          "Boot",         "KiCad"
+        "CMD+C",        "CMD+V",        "CMD+X",        "CMD+Z",
+        "CMD+A",        "CMD+S",        "CMD+F",        "CMD+SHFT+Z",
+        "CMD+SHFT+3",   "CMD+SHFT+4",   "Bootloader",   "KiCad"
     },
     {
         "R",            "M",            "G",            "DEL",
@@ -38,19 +38,19 @@ static const char* key_names[MAX_LAYERS][MAX_POSITIONS] = {
         "B",            "F",            "H",            "Fusion"
     },
     {
-        "⌘K",           "R",            "L",            "D",
-        "E",            "Q",            "F",            "⌘Z",
-        "⌘1",           "⌘2",           "⌘0",           "VSCode"
+        "Cmd+K",        "R",            "L",            "D",
+        "E",            "Q",            "F",            "CMD+Z",
+        "CMD+1",        "CMD+2",        "CMD+0",        "VS Code"
     },
     {
-        "⌘P",           "⌘⇧P",          "⌘B",           "⌘/",
-        "⌘D",           "⌘⇧L",          "⌘F",           "⌘Z",
-        "⌘`",           "⌘⇧F",          "⌘⇧E",          "Arduino"
+        "Cmd+P",        "CMD+SHFT+P",   "CMD+B",        "CMD+FSLH",
+        "CMD+D",        "CMD+SHFT+L",   "CMD+F",        "CMD+Z",
+        "CMD+SQRT",     "CMD+SHFT+F",   "CMD+SHFT+E",   "Arduino"
     },
     {
-        "⌘R",           "⌘U",           "⌘⇧M",          "⌘K",
-        "⌘T",           "⌘Z",           "⌘L",           "⌘S",
-        "BT CLR",       "BT NXT",       "Reset",        "Base"
+        "CMD+R",        "CMD+U",        "CMD+SHFT+M",   "CMD+K",
+        "CMD+T",        "CMD+Z",        "CMD+L",        "CMD+S",
+        "BT Clear",     "BT Next",      "Reset",        "Base"
     }
 };
 
